bb9dfd3ce0c9838628a98975950e0f1c76a24393
Oscar Kemuel <oscar.kemuel5@gmail.com>
2022-09-26 13:02:30 -0300

    feat: implements CRUD of Store

--- app/Controllers/Http/StoresController.ts
+++ app/Controllers/Http/StoresController.ts
@@ -1,19 +1,49 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import StoreService from 'App/Services/StoreService'
 
 export default class StoresController {
-  public async details({ view }: HttpContextContract) {
-    return view.render('store/details')
+  public storeService = new StoreService()
+
+  public async details({ view, request }: HttpContextContract) {
+    const id = request.param('id')
+    const store = await this.storeService.getStoreById(id)
+
+    return view.render('store/details', { store })
   }
 
   public async list({ view }: HttpContextContract) {
-    return view.render('store/list')
+    const stores = await this.storeService.getAllStores()
+
+    return view.render('store/list', { stores })
   }
 
   public async showForm({ view }: HttpContextContract) {
     return view.render('store/form')
   }
 
-  public async edit({ view }: HttpContextContract) {
-    return view.render('store/edit')
+  public async create({ request, response, session }: HttpContextContract) {
+    const data = request.only(['name', 'description', 'address'])
+
+    await this.storeService.createStore(data)
+
+    session.flash('success', 'Loja cadastrada com sucesso!')
+    return response.redirect().toRoute('/loja')
+  }
+
+  public async edit({ view, request }: HttpContextContract) {
+    const id = request.param('id')
+    const store = await this.storeService.getStoreById(id)
+
+    return view.render('store/edit', { store })
+  }
+
+  public async update({ request, response, session }: HttpContextContract) {
+    const data = request.only(['name', 'description', 'address'])
+    const id = request.param('id')
+
+    await this.storeService.updateStore(data, id)
+
+    session.flash('success', 'Loja atualizada com sucesso!')
+    return response.redirect().toRoute('/loja')
   }
 }
---
+++ app/Services/StoreService.ts
@@ -0,0 +1,41 @@
+import Store from 'App/Models/Store'
+
+interface StoreDataPayload {
+  name: string
+  description: string
+  address: string
+}
+
+class StoreService {
+  public async createStore(data: StoreDataPayload) {
+    const store = await Store.create(data)
+
+    return store
+  }
+
+  public async updateStore(data: StoreDataPayload, id: number) {
+    const store = await Store.findOrFail(id)
+
+    store.name = data.name
+    store.description = data.description
+    store.address = data.address
+
+    await store.save()
+
+    return store
+  }
+
+  public async getStoreById(id: number) {
+    const store = await Store.findOrFail(id)
+
+    return store
+  }
+
+  public async getAllStores() {
+    const stores = await Store.all()
+
+    return stores
+  }
+}
+
+export default StoreService
